09c4573e92ef1eb0af79aeb15b67f253021bcb40
kklein <kevin.klein@quantco.com>
2024-01-05 10:51:00 +0100

    Add a header to csv file.

--- export_submissions.sh
+++ export_submissions.sh
@@ -3,8 +3,12 @@
 for year in "2021" "2022" "2023"
 do
     echo $year
+    # Create a header in csv file.
+    echo "day,n_submissions" >> data/submissions/$year.csv
+
     entire_html=$(curl https://adventofcode.com/$year/stats)
     days=$(echo $entire_html | grep -o "href=\"/${year}/day/[^\"]*" | sed -n "s|href=\"/${year}/day/||p")
+
     for day in $days
     do
 	submissions=$(echo $entire_html | grep -o "> *${day} <span class=\"stats-both\">[^<]*" | sed -n "s|> *${day} <span class=\"stats-both\">||p")

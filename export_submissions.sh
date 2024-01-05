5520412d30c683948a60e929ea0d8e6c89477add
kklein <kevin.klein@quantco.com>
2024-01-05 10:25:43 +0100

    Draft export script for submissions.

---
+++ export_submissions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for year in "2021" "2022" "2023"
+do
+    echo $year
+    entire_html=$(curl https://adventofcode.com/$year/stats)
+    days=$(echo $entire_html | grep -o "href=\"/${year}/day/[^\"]*" | sed -n "s|href=\"/${year}/day/||p")
+    for day in $days
+    do
+	submissions=$(echo $entire_html | grep -o "> *${day} <span class=\"stats-both\">[^<]*" | sed -n "s|> *${day} <span class=\"stats-both\">||p")
+	echo "$day,$submissions" >> data/submissions/$year.csv
+    done
+done
